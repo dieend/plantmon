e1edcce9a25f3d892e3de5491488af30def9143e
dieend1@gmail.com <dieend1@gmail.com@d4cce1eb-ffa5-2f15-553d-1ef65b512411>
2011-04-09 16:23:09 +0000

    Item yang diperbaiki. FarmItem dan WarItem merupakan turunan dari Item. dan tiap dari mereka punya method static ItemFactory, yang tentu saja kalau dari kelas FarmItem untuk ngebuat item farm, yang dari kelas WarItem ngebuat item war. Kata Onta kemaren item war itu untuk pulmosis, jadi atributnya mungkin damage, bisa positif atau negatif. Kalau positif ngurangin hape, kalau negatif nambahin hape. Tapi ntar kita diskusiin lagi.

--- trunk/entity/Item.java
+++ trunk/entity/Item.java
@@ -1,11 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+package plantmon.entity;
 
-package plantmon.entity.item;
-
-// maaf yang item masih belum -.-
 public class Item{
     String name;
     int costBuy;
