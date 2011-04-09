82ce70505ab5440bd190120bb1274e42d36cb8d7
dieend1@gmail.com <dieend1@gmail.com@d4cce1eb-ffa5-2f15-553d-1ef65b512411>
2011-04-09 16:23:09 +0000

    Item yang diperbaiki. FarmItem dan WarItem merupakan turunan dari Item. dan tiap dari mereka punya method static ItemFactory, yang tentu saja kalau dari kelas FarmItem untuk ngebuat item farm, yang dari kelas WarItem ngebuat item war. Kata Onta kemaren item war itu untuk pulmosis, jadi atributnya mungkin damage, bisa positif atau negatif. Kalau positif ngurangin hape, kalau negatif nambahin hape. Tapi ntar kita diskusiin lagi.

--- entity/item/WarItem.java
+++ entity/item/WarItem.java
@@ -1,6 +1,8 @@
 package plantmon.entity.item;
 
-public class WarItem{
+import plantmon.entity.Item;
+
+public class WarItem extends Item{
     int tipeWarItem;
     int tipePeluru;
     int costImproveItem;
@@ -24,21 +26,9 @@ public class WarItem{
     public void setCostImproveItem(int e){
         costImproveItem=e;
     }
-    public Item Item(String name) {
-        Item item = new Item();
-        FarmItem farmitem = new FarmItem();
-        if(name.equals("kentang"))      {item.setIDitem(0);farmitem.setTypeTanaman(1);farmitem.setTypeBuah(13);farmitem.setEfekBuah(1);item.setCostBuy(150);item.setCostSell(720);farmitem.setCostSellBibit(75);farmitem.setBibit(true);}
-        else if(name.equals("lobak"))	{item.setIDitem(1);farmitem.setTypeTanaman(2);farmitem.setTypeBuah(14);farmitem.setEfekBuah(2);item.setCostBuy(120);item.setCostSell(540);farmitem.setCostSellBibit(60);farmitem.setBibit(true);}
-        else if(name.equals("timun"))	{item.setIDitem(2);farmitem.setTypeTanaman(3);farmitem.setTypeBuah(15);farmitem.setEfekBuah(1);item.setCostBuy(200);item.setCostSell(900);farmitem.setCostSellBibit(100);farmitem.setBibit(true);}
-        else if(name.equals("kubis"))	{item.setIDitem(3);farmitem.setTypeTanaman(4);farmitem.setTypeBuah(16);farmitem.setEfekBuah(4);item.setCostBuy(500);item.setCostSell(2250);farmitem.setCostSellBibit(250);farmitem.setBibit(true);}
-        else if(name.equals("jagung"))	{item.setIDitem(4);farmitem.setTypeTanaman(5);farmitem.setTypeBuah(17);farmitem.setEfekBuah(1);item.setCostBuy(300);item.setCostSell(900);farmitem.setCostSellBibit(150);farmitem.setBibit(true);}
-        else if(name.equals("tomat"))	{item.setIDitem(5);farmitem.setTypeTanaman(6);farmitem.setTypeBuah(18);farmitem.setEfekBuah(1);item.setCostBuy(200);item.setCostSell(540);farmitem.setCostSellBibit(100);farmitem.setBibit(true);}
-        else if(name.equals("bawang"))	{item.setIDitem(6);farmitem.setTypeTanaman(7);farmitem.setTypeBuah(19);farmitem.setEfekBuah(2);item.setCostBuy(150);item.setCostSell(720);farmitem.setCostSellBibit(75);farmitem.setBibit(true);}
-        else if(name.equals("nanas"))	{item.setIDitem(7);farmitem.setTypeTanaman(8);farmitem.setTypeBuah(20);farmitem.setEfekBuah(4);item.setCostBuy(1000);item.setCostSell(4500);farmitem.setCostSellBibit(500);farmitem.setBibit(true);}
-        else if(name.equals("wortel"))	{item.setIDitem(8);farmitem.setTypeTanaman(9);farmitem.setTypeBuah(21);farmitem.setEfekBuah(1);item.setCostBuy(300);item.setCostSell(1080);farmitem.setCostSellBibit(150);farmitem.setBibit(true);}
-        else if(name.equals("terong"))	{item.setIDitem(9);farmitem.setTypeTanaman(10);farmitem.setTypeBuah(22);farmitem.setEfekBuah(4);item.setCostBuy(120);item.setCostSell(720);farmitem.setCostSellBibit(60);farmitem.setBibit(true);}
-        else if(name.equals("ubi"))     {item.setIDitem(10);farmitem.setTypeTanaman(11);farmitem.setTypeBuah(23);farmitem.setEfekBuah(2);item.setCostBuy(300);item.setCostSell(1080);farmitem.setCostSellBibit(150);farmitem.setBibit(true);}
-        else if(name.equals("paprika"))	{item.setIDitem(11);farmitem.setTypeTanaman(12);farmitem.setTypeBuah(24);farmitem.setEfekBuah(4);item.setCostBuy(150);item.setCostSell(360);farmitem.setCostSellBibit(75);farmitem.setBibit(true);}
+    public static WarItem ItemFactory(String name) {
+        WarItem item = new WarItem();
+        if(name.equals("armor1"))      {item.setIDitem(71);}
         else {item = null;}
         return item;
     }
