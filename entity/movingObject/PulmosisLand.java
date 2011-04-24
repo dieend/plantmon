df7f929707fdbf0a82a6d20c21da9b87996f2aeb
dieend1@gmail.com <dieend1@gmail.com@d4cce1eb-ffa5-2f15-553d-1ef65b512411>
2011-04-24 05:27:49 +0000

    Ada nemu beberapa bug dan udah diperbaiki: 1. player bisa cangkul/nanam/water/slash di daerah yang ga bisa dijangkaunya. 2. kalo talk/water pulmosis cancelnya ga hilang. 3. Nanam kentang ga bisa.
    Mungkin masih ada bug-bug lainnya, tolong kasi tau ya. kirim ke milis atau gimana....
    Nyobainnya pake save load. Di sebelah kiri kasur ada buku bisa untuk save dan load. (cuma 1)

--- entity/movingObject/PulmosisLand.java
+++ entity/movingObject/PulmosisLand.java
@@ -75,6 +75,7 @@ public class PulmosisLand extends PulmosisBattle implements Cancellable,
                 }
             }
             if (!cancel[0]){
+                map.popCancel(gx, gy);
                 sumWater += 1;
                 watered = true;
             }
